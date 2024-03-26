e09e3bf9fe1f2ef12d53f34a3c97e2abf4607514
Juanse1108 <juansetonog@gmail.com>
2024-03-26 15:00:53 -0500

    se agrega API para actualizar documentos y token en login

--- src/controllers/Documents.controller.ts
+++ src/controllers/Documents.controller.ts
@@ -30,3 +30,29 @@ export const uploadFile: RequestHandler = async(req, res) => {
     }
 }
 
+export const updateFileController: RequestHandler = async(req, res) => {
+  try {
+      const { blobName, idDocument } = req.body;
+      if (!req.files || Object.keys(req.files).length === 0) {
+          res.status(400).send("No files were uploaded.");
+          return;
+        }
+        if (!req.files.filePath) {
+          res.status(400).send("No files were uploaded.");
+          return;
+        }
+        const filePath = req.files.filePath as UploadedFile
+        filePath.mv('./tmp/' + filePath.name, function(err) {
+          if (err)
+            return res.status(500).send(err);
+        });
+
+        const { code, message, ...resto }: DocumentsRepositoryService = await repository.updateFileRepository(`./tmp/${filePath.name}`, blobName, idDocument);
+
+
+        res.status(code).json({ message: parseMessageI18n(message, req), ...resto });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: parseMessageI18n('error_server', req) });  
+  }
+}
---
+++ src/helpers/generateJWT.ts
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+
+export const generateJWT = (uid: any = "", time: string = "15m") => {
+  const secret: any = process.env.SECRET_KEY || 'givesharingfood';
+  return new Promise((resolve, reject) => {
+    const payload = { uid };
+
+    jwt.sign(payload, secret, { expiresIn: time }, (err: any, token: any) => {
+      if (err) {
+        console.error('token: ', err);  
+        reject("No se generó el Token");
+      } else {
+        resolve(token);
+      }
+    });
+  });
+};
+
+export const parseJwt = async (token: any) => {
+  const secret: any = process.env.SECRET_KEY || 'givesharingfood';
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (err: any, decoded: any) => {
+      if (err) {        
+        reject("No se generó el Token");
+      } else {
+        // let date = new Date();        
+        // date.setTime(decoded.exp * 1000);
+        resolve(decoded.exp * 1000);
+      }
+    });
+  });
+};
+
+export const verifyJwt = async (token: any) => {
+  const secret: any = process.env.SECRET_KEY || 'givesharingfood';
+  
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (err: any, decoded: any) => {
+      if (err) {        
+        reject("El Token ya expiró");
+      } else {
+        resolve('Token vigente');
+      }
+    });
+  });
+};
