64d63407861bae7a321130639a78166eef7f2e88
Juanse1108 <juansetonog@gmail.com>
2024-07-04 18:18:16 -0500

    se ajusta ruta en user.Router

--- src/routes/User.Router.ts
+++ src/routes/User.Router.ts
@@ -13,14 +13,15 @@ userRouter.post(
         body("phone","user.required_field_text").isString().notEmpty(),
         body("email", "user.validate_email").notEmpty().isEmail(),
         body("email").custom(validateEmailUserExist),
-        body("password", "user.required_field_text").isString().notEmpty,
+        body("password", "user.required_field_text").isString().notEmpty(),
         body("googleAddress","user.required_field_text").notEmpty().isString(),
         body("idOrganization","user.validate_field_int").notEmpty().isInt(),
         body("idCity","user.validate_field_int").notEmpty().isInt(),
         body("idDepartmen","user.validate_field_int").notEmpty().isInt(),
         validateEnpoint
     ],
-    createUser);
+    createUser
+);
 
 userRouter.get(
     "/users/:idOrganization",
